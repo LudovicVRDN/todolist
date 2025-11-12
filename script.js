2a852d1cc454369651d8838237f9aaf311b5e514
LudovicVRDN <vourdon@live.fr>
2025-11-12 14:00:32 +0100

    add : checkbox

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+/* ajout de la fonction du popup*/
+
 function togglePopup(){
     let popup = document.querySelector("#popupoverlay");
     popup.classList.toggle("open")
 }
 
+/*Changement de couleur des boutons*/
 
+let boutons = document.querySelectorAll('.bouton');
 
-
-    
-    
+boutons.forEach(bouton => {
+    boutons.addEventListener('click',() => {
+        boutons.classList.toggle('fait');
+    });
+});
