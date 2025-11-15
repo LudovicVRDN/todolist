a39904777da39137ac1458cbd3dc36d28d4b622e
LudovicVRDN <vourdon@live.fr>
2025-11-15 08:13:09 +0100

    add : fin js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-/* ajout de la fonction du popup*/
+//ETAPE 1 : initialisation des variables
+let ajout = document.querySelector(".buttonajout");
 
+//ETAPE 2 : declaration des fonctions
 
-let popup = document.querySelector("#popupoverlay");
+function addNote(){
+    //a l'ajout d'une note
+}
+
+function getID(){
+    const ID = JSON.parse(localStorage.getItem("ID")) || 0;
+    localStorage.setItem("ID", JSON.stringify(ID+1));
+    return ID;
+}
+
 function togglePopup() {
-
+    let popup = document.querySelector("#popupoverlay");
     popup.classList.toggle("open")
 }
 
 function removeclass() {
+    let popup = document.querySelector("#popupoverlay");
     popup.classList.remove("open");
 };
-
-/*ajout de la fonction de création des notes */
-const local = JSON.parse(localStorage.getItem("notes"));
-
-let ajout = document.querySelector(".buttonajout");
-let annuler = document.querySelector(".buttonannuler");
 
 ajout.onclick = () => {
     const note = {
@@ -26,49 +32,46 @@ ajout.onclick = () => {
         fait: fait.value,
         description: description.value,
         remarque: remarques.value,
+        id: getID()
     };
-    localStorage.setItem("notes", JSON.stringify(note));
+
+
+    //attention, on doit pouvoir ajouter. Actuellement on ecrase.
+    /*
+        1. recup les données du localStorage (bien sous la forme d'une liste)
+        2. ajouter la note qu'on vient de créer a la liste
+        3. enregistrer dans le localStorage la liste mise a jour
+    */
+    json = localStorage.getItem("notes"); //ca marche pas, attention a bien transformer en liste
+
+    if(json == null){
+        listeNotes = [];
+    }else{
+        listeNotes = JSON.parse(json);
+    }
+    listeNotes.push(note)
+
+    localStorage.setItem("notes", JSON.stringify(listeNotes));
+
     document.location.reload();
 }
 
-let titrenote = document.querySelector(".titrenotes")
+function addtoHTLM(){
+    let local = JSON.parse(localStorage.getItem("notes"));
+    let todolist = document.querySelector('#todo');
+
+    local.forEach(note => {
+        /* test création des notes*/
+        let li = document.createElement("li");
+
+        li.innerHTML = `Titre de la note : ${note.titre} Date de début : ${note.datededebut}  Date de fin : ${note.datefin} <br>
+        Description: <br> ${note.description} <br> Remarques : <br> ${note.remarque} 
+        <input type="checkbox" , class="checkboxround" name="faitpasfait" value="off">`;
+
+        todolist.appendChild(li);
+    });
+
+}
 
 
-/* test création des notes*/
-const todolist = document.querySelector('#todo');
-const li = document.createElement("li");
-
-li.innerHTML = `Titre de la note : ${local.titre} Date de début : ${local.datededebut}  Date de fin : ${local.datefin} <br>
-Description: <br> ${local.description} <br> Remarques : <br> ${local.remarque} 
-<input type="checkbox" , class="checkboxround" name="faitpasfait" value="off">`;
-
-todolist.appendChild(li);
-
-/*fonction pour valider la checkbox */
-let checkbox = document.querySelectorAll(".checkboxround");
-let casfait = document.getElementById("fait");
-
-
-/* FONCTIONS POUR LA TO DO LIST //
-
-Fonction du bouton "Ajouter" : 
-1)Ouvrir le pop up pour créer une note; X
-2) Créer des notes (setItem); X
-3)Les lire (getItem); X
-4)Remplir les notes avec la valeur du localStorage correspondante; X
-5)Créer individuellement les notes (si 1 note crée seulement 1 note affichée si 99 notes crées affichées les 99);
-
-Fonctions pour le bouton "Affichage priorité":
-1)Filtrer les notes soit par importances soit par date;
-2)Changer l'affichage en fonction du mode choisi;
-
-Fonctions pour les notes:
-1)Pouvoir consulter la note;
-2)Pouvoir modififer la note tant qu'elle n'est pas terminée;
-3)Pouvoir supprimer la note;
-4)Ne peux plus pouvoir modifier la note une fois qu'elle est terminée sauf la remarque;
-5)Ouverture d'un pop up pour enregistrer la modification;
-6)Ouverture d'un pop up pour valider la suppression;
-
-Fonction pour la checkbox:
-1)Si la checkbox est "checked" alors la note doit etre considérée comme faites (fait = on); */
+addtoHTLM();
