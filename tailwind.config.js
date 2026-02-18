b0367584e5a9794730858ed7864ccb5775c2c112
LudovicVRDN <vourdon@live.fr>
2026-02-18 18:27:55 +0100

    Begin new version with OOP .

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#da373d",
+        secondary: "#22F",
+        colorLight: "#EEE",
+      },
+    },
+  },
+  plugins: [],
+};
